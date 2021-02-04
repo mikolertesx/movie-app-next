b96f25d6b92d3ed7e687b03cdf5b83df3642e4cc
Mike <mgangsal@outlook.com>
2021-02-04 15:08:53 -0600

    Finished sketch for the app

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+// const About = () => {
+//   const message = "Hello world!";
+//   return React.createElement(
+//     "div",
+//     null,
+//     "I am generating this with createElement"
+//   );
+// };
+
+class About extends React.Component {
+  render() {
+    return <h1>Hello I am class component</h1>;
+  }
+}
+
+export default About;
